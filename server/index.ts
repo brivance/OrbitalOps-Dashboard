0d83775c8c943707706dd460db397a4cd08057f6
Brianna White <19briannawhite@gmail.com>
2026-06-04 09:30:09 -0700

    fixed an error in the tests. all should pass now

--- server/index.ts
+++ server/index.ts
@@ -164,7 +164,7 @@ const yoga = createYoga({
     resolvers,
   }),
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: false,
   },
 })
